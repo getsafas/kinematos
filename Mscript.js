290897543a78c3b31ebd1e304e1d6e73c07b96b8
= <getsafas@gmail.com>
2015-11-19 13:22:46 +0530

    updated fully functional

--- Mscript.js
+++ Mscript.js
@@ -5,9 +5,6 @@ $(function() {
 		if($('#movie-name').val() == ''){
 			return false;
 		}
-		// $('#movieInput button').html('Loading...').addClass('active disabled');
-		// $('#movieInput input').attr('disabled',true);
-
 		$.ajax({
 			url: 'http://www.omdbapi.com',
 			type: 'get',
@@ -19,33 +16,52 @@ $(function() {
 			},
 		})
 		.done(function(data) {
-			$('.movieFeeds').slideUp();
-			
+
 			$('.movieResults .name-tile .movie-name').html(data.Title);
 			$('.movieResults .name-tile .movie-year').html(data.Year);
 			$('.movieResults .name-tile .movie-genre').html(data.Genre);
 			$('.movieResults .name-tile .movie-language').html(data.Language);
-			
+
 			$('.movieResults .actor-tile .movie-actor').html(data.Actors);
 			$('.movieResults .actor-tile .movie-director').html(data.Director);
 			$('.movieResults .actor-tile .movie-writer').html(data.Writer);
 			$('.movieResults .actor-tile .movie-awards').html(data.Awards);
-			
+
 			$('.movieResults .imdb-tile .movie-imdb-rating').html(data.imdbRating);
 			$('.movieResults .imdb-tile .movie-imdb-score').html(data.imdbScore);
 			$('.movieResults .imdb-tile .movie-imdb-votes').html(data.imdbVotes);
 			$('.movieResults .imdb-tile .movie-metascore').html(data.Metascore);
-			
+
 			$('.movieResults .plot-tile .movie-plot-data').html(data.Plot);
-			
-			$('.movieResults').slideDown();
-			console.log(data);
-			console.log("success");
+
+			$('.movieFeeds').slideUp(500, function(){
+				$('.movieResults').slideDown();
+			});			
 		})
-		.fail(function() {
-			console.log("error");
-		})
+.fail(function() {
+	console.log("error");
+})
+});
+});
+
+$(function(){
+	$('#back').click(function(){
+		$('.movieSearch input').val("");
+		
+		$('.movieResults').slideUp(500, function(){
+			$('.movieFeeds').slideDown();
+		});
 	});
 });
+
+$(function(){
+	$('.action-more').click(function(){
+		$('#movie-name').val($(this).data('movie'));
+		$('#movieInput').trigger('submit');
+		console.log("done");
+	});
+});
+
+
 
 
